b0b4383806fc8ef3f075467a77d0b38d34b98777
Fazlur Rahman Khan <fyndfrk@gmail.com>
2022-06-20 15:48:17 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "filename" {
+  default = "/root/myproject/abc.txt"
+}
+
+variable "content" {
+  default = "Welcome to TF"
+}
+
+variable "prefix" {
+     }
+
+variable "separator" {
+  default = "."
+}
+variable "length" {
+  default = "2"
+}
