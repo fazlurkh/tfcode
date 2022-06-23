4d4dfa2f17941b7744237ddf27bab060d63e175a
Fazlur Rahman Khan <fyndfrk@gmail.com>
2022-06-23 23:45:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+   access_key = "AKIAXDG6I4AHOAHHMPG2"
+  secret_key = "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"
 }
 
 resource "aws_instance" "my-instance" {
